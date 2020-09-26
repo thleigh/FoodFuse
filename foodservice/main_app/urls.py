cb6108609a85aa9f19d78fb2cae189e4100f9a78
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-26 14:16:29 -0700

    add auth for superuser

--- foodservice/main_app/urls.py
+++ foodservice/main_app/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('user/<username>/', views.profile, name='profile'),
+]
--- foodservice/main_app/views.py
+++ foodservice/main_app/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-
-# from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 # from .models import Cat, CatToy
-# from django.views.generic.edit import CreateView, UpdateView, DeleteView
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-# from django.contrib.auth import authenticate, login, logout
-# from django.contrib.auth.decorators import login_required
-# from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+# DEFAULT
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+  return render(request, 'index.html')
