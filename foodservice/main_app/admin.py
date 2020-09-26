9841cb95aa3cd0f3cd5a4b36f6c5956c038d20d5
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-26 13:21:42 -0700

    add boilerplate template

---
+++ foodservice/main_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+# from .models import Cat, CatToy
+
+# Register your models here.
---
+++ foodservice/main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# from django.shortcuts import render, redirect
+# from .models import Cat, CatToy
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.http import HttpResponseRedirect, HttpResponse
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+
+# Create your views here.
