af433fd7fa5516443ba1d54f9a8cd31ba54b2663
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-29 20:35:34 -0700

    [postmates] show postmates data

--- foodservice/main_app/forms.py
+++ foodservice/main_app/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SearchForm(forms.Form):
     location = forms.CharField()
-    # restaurant = forms.CharField()
 
+class RestaurantForm(forms.Form):
+    restaurant = forms.CharField()
 
