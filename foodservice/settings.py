5560f2810820c4db13c4bb05968a4db76310ee5c
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-01 14:07:21 -0700

    [file] move foodservice into foodservice

--- foodservice/settings.py
+++ foodservice/settings.py
@@ -115,8 +115,8 @@ DATABASES = {
     }
 }
 
-# #production
-# DATABASES['default'] = dj_database_url.congif(conn_max_age=600, ssl_require=True)
+#production
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
